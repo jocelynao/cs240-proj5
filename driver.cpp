1edac5c61bd1d5711e4186f185da50e362cefad7
Ryan Sharpelletti <rsharpe1@binghamton.edu>
2017-12-02 00:52:34 -0500

    added driver testing

--- driver.cpp
+++ driver.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <sstream>
 #include <string>
+#include <unordered_map>
+#include "offeringsParser.h"
 
 using namespace std;
 
@@ -9,7 +11,15 @@ int main(int argc, char **argv){
 	if(argc != 4){
 		cout << "Please run the program as follows: ./schedule.exe <Requirements File> <Offerings File> <Planned Schedule File>" << endl;
 	}else{
-		cout << "doing stuff" << endl;
+		unordered_map<string, offerings*> offersMap;
+		offeringsParser *offers = new offeringsParser(argv[2]);
+		offersMap = offers -> getMap();
+
+		
+		auto search = offersMap.find("AN301");
+		if(search != offersMap.end()){
+			cout << "Found " << search -> first << " : " << search -> second -> getCredits();
+		}
 	}
 	return 0;
 }
